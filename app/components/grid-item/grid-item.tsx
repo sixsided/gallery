17a9ad455fd524f91a8ddd836ef0ce0985423c06
sixsided <>
2024-06-04 00:06:32 -0700

    components

---
+++ app/components/grid-item/grid-item.tsx
@@ -0,0 +1,30 @@
+import { Link } from '@remix-run/react';
+import ExpandIcon from '../expand-icon/expand-icon';
+import './grid-item.css';
+
+const dims = ({ width, height }) => {
+  return `${width} x ${height}`;
+};
+
+type GridItemProps = {
+  thumbnailUrl: string;
+  largeUrl: string;
+  originalUrl: string;
+  name?: string;
+  dimensions?: { width:number; height:number };
+};
+
+export default ({ thumbnailUrl, largeUrl, originalUrl, name, dimensions }: GridItemProps) => {
+  const thumbnailCssUrl = `url("${thumbnailUrl}")`;
+  return (
+    <div className="grid-item">
+      <Link to={largeUrl}>
+        <div className="grid-item__thumbnail" style={{ backgroundImage: thumbnailCssUrl }}></div>
+      </Link>
+      <a className="grid-item__tools no-tufte-underline" target="_blank" href={originalUrl} title={name}>
+        <ExpandIcon />
+        {dimensions && <div>{dims(dimensions)}</div>}
+      </a>
+    </div>
+  );
+};
---
+++ app/components/image-grid/image-grid.tsx
@@ -0,0 +1,25 @@
+import './image-grid.css';
+import { DbFile } from '../../interfaces';
+import GridItem from '../grid-item/grid-item';
+
+
+const resize = `-/resize/x240`;
+const imgUrl = (uuid, tweak) => `https://ucarecdn.com/${uuid}/${tweak}/`;
+const thumbnail = (uuid) => imgUrl(uuid, resize);
+
+export function ImageGrid({ images }: { images: DbFile[] }) {
+  return (
+    <div className="image-grid">
+      {[...images].map((x) => (
+        <GridItem
+          key={x.uuid}
+          name={x.originalFilename || 'untitled'}
+          thumbnailUrl={thumbnail(x.uuid)}
+          largeUrl={`/photos/${x.uuid}`}
+          originalUrl={x['originalFileUrl'] || '#'}
+          dimensions={x.contentInfo?.image}
+        />
+      ))}
+    </div>
+  );
+}
