48db097f0a104be2ecdb938b01aa244c5ba9dd93
sixsided <>
2024-06-04 00:02:08 -0700

    basics

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 /// <reference types="vitest" />
-import { defineConfig } from 'vitest/config'
+// import { defineConfig } from 'vitest/config'
+import { vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // https://remix.run/docs/en/main/start/quickstart
+  plugins: [remix()],
   test: {
     /* for example, use global to avoid globals imports (describe, test, expect): */
     globals: true,
